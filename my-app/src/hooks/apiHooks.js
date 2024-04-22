a8b00cab074c017c7c49675818c0a9161859467f
Samuli Lamminmäki <36449245+SamuliLam@users.noreply.github.com>
2024-04-22 15:30:06 +0300

    Forms task done

---
+++ my-app/src/hooks/apiHooks.js
@@ -0,0 +1,71 @@
+import {useEffect, useState} from "react";
+import {fetchData} from "../lib/fetchData.js";
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const {getUserById} = useUser();
+  const getMedia = async () => {
+    const mediaResult = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
+
+    const mediaWithUser = await Promise.all(mediaResult.map(async (item) => {
+      const userResult = await getUserById(item.user_id);
+      return {...item, username: userResult.username};
+    }));
+    setMediaArray(mediaWithUser);
+  }
+
+  useEffect(() => {
+    getMedia();
+  }, [])
+  return {mediaArray};
+};
+
+const useUser = () => {
+  const getUserById = async (id) => {
+    const userResult = await fetchData(import.meta.env.VITE_AUTH_API + '/users/' + id);
+    return userResult;
+  }
+
+  const getUserByToken = async (token) => {
+    const options = {
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+    }
+    const tokenResult = await fetchData(
+      import.meta.env.VITE_AUTH_API + '/users/token',
+      options
+    );
+    return tokenResult;
+  }
+  const register = async (inputs) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputs)
+    }
+
+    return await fetchData(import.meta.env.VITE_AUTH_API + '/users', options);
+  }
+
+  return {getUserById, getUserByToken, register};
+}
+
+const useAuthentication = () => {
+  const login = async (inputs) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputs)
+    }
+    const loginResult = await fetchData(import.meta.env.VITE_AUTH_API + '/auth/login', options);
+    return loginResult;
+  }
+  return {login}
+}
+
+export {useMedia, useUser, useAuthentication};
---
+++ my-app/src/views/Login.jsx
@@ -0,0 +1,22 @@
+// imports
+import LoginForm from "../components/LoginForm.jsx";
+import RegisterForm from "../components/RegisterForm.jsx";
+import {useState} from "react";
+import Button from "../components/UI/Button.jsx";
+
+const Login = () => {
+  const [toggleForm, setToggleForm] = useState(true);
+
+  const toggle = () => {
+    setToggleForm(!toggleForm);
+  }
+
+  return (
+    <>
+      {toggleForm ? <LoginForm/> : <RegisterForm/>}
+      <Button text={toggleForm ? 'Not registered yet?' : 'Go to login'} handleClick={toggle}/>
+    </>
+  );
+};
+
+export default Login;
